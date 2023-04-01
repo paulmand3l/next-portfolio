9a08e091376379eed6a5653a5a2a85c82a540a4f
Paul Mandel <paul.mand3l@gmail.com>
2023-04-01 00:02:23 -0700

    sketching in main areas

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withStylus = require('next-stylus');
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withStylus(nextConfig)
